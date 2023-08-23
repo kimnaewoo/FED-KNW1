f415be14a0018668e6d8e7d092faea363f240d2a
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-08-23 13:19:26 +0900

    Update for_ex.js

--- 003.JS학습/js/for_ex.js
+++ 003.JS학습/js/for_ex.js
@@ -11,8 +11,20 @@ var blue_box = document.querySelector('.Bcase');
 
 console.log('미니언즈:',mini,blue_box);
 
-// 2. 이벤트 연결하기 
+// 2. 이벤트 연결하기 /////////////////////////////////////////////
 
+// 2-1. 미니언즈 이미지 클릭시 넣기함수 호출 
+// mini[0].onclick = insertMini;
+
+// 기본 for문으로 반복하여 개수만큼 이벤트셋팅하기
+// for(시;한;증){코드}
+// (0부터 시작; 개수보다 작을때까지 반복; 1씩 증가)
+for(var i=0;i<mini.length;i++){
+    mini[i].addEventListener('click',insertMini);
+    console.log('for문내i:',i);
+} // for문 /////////////////////////
+
+console.log('for문밖i:',i);
 
 // 3. 함수 만들기 /////////////////////////////////////////////////
 
@@ -24,5 +36,22 @@ console.log('미니언즈:',mini,blue_box);
 *************************************************/
 function insertMini(){
     // 1. 호출확인
-    console.log('미니넣어');
+    // console.log('미니넣어',this);
+    // this 나자신! -> 호출한 .mini 
+
+    // 2. 셋팅된 개수 속성(data-cnt) 가져오기
+    var cnt = this.getAttribute('data-cnt');
+    // getAttribute(속성명) - 속성값 가져오기! 내장함수 
+    // console.log('data-cnt:',cnt);
+
+    // 3. 미니언즈 넣기
+    // 대상 : .Bcase -> Blue_box
+    // for(시;한;증){코드}
+    // (0부터;개수보다작을때까지;1씩증가)
+    for(var j=0;j<cnt;j++){
+        blue_box.innerHTML += `
+        <img src="./images/Minions.png"
+        alt="미니언즈">
+        `;
+    } // for /////////////////////////
 } // insertMini 함수//////////////////////////////////////////////
