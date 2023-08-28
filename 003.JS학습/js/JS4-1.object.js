64ce52cb2289363e9ee6b531f186d3c56072bae3
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-08-28 16:58:33 +0900

    s

--- 003.JS학습/js/JS4-1.object.js
+++ 003.JS학습/js/JS4-1.object.js
@@ -135,29 +135,29 @@ seo_obj.trailer = function () {
     playMovie("HuQ6YFDYY0s");
 }; /////////트레일러 함수//////////
 // 박정현님의 오브젝트!
-const jh_obj = {}; 
+const jh_obj = {};
 // 오브젝트 형만 만들고 할당은 아래에서!
 // 1. 영화제목
-jh_obj.title = 'Her';
+jh_obj.title = "Her";
 
 // 2. 감독
-jh_obj.director = '스파이크 존즈';
+jh_obj.director = "스파이크 존즈";
 
 // 3. 배우
-jh_obj.actor = '호아킨 피닉스';
+jh_obj.actor = "호아킨 피닉스";
 
 // 4. 장르
-jh_obj.genre = '드라마/로맨스';
+jh_obj.genre = "드라마/로맨스";
 
 // 5. 관람가
-jh_obj.ratings = '15세 관람가';
+jh_obj.ratings = "15세 관람가";
 
 // 6. 예고편
-jh_obj.trailer = function(){
-    console.log('예고편:','pa54j-65rGQ');
+jh_obj.trailer = function () {
+    console.log("예고편:", "pa54j-65rGQ");
     // 플레이어함수 호출!
-    playMovie('pa54j-65rGQ');
-}///// trailer 함수 //////// 
+    playMovie("pa54j-65rGQ");
+}; ///// trailer 함수 ////////
 // 김민정의 오브젝트!
 const kmj_obj = {};
 // 오브젝트 형만 만들고 객체내용은 아래에서!
@@ -182,147 +182,146 @@ kmj_obj.trailer = function () {
     // 플레이어함수 호출!
     playMovie("XyHr-s3MfCQ");
 }; ///////// trailer 함수 ///////
- // 임경은님의 오브젝트 
-            
- const lke_obj = {};
- // 오브젝트 형만 만들고 할당은 아래에서
- // 1. 영화제목
- lke_obj.title = 'WALL·E';
- // 2. 감독
- lke_obj.director = '앤드루 스탠튼';
- // 3. 배우
- lke_obj.actor = '벤 버트, 엘리사 나이트외';
- // 4. 장르
- lke_obj.genre = '애니메이션';
- // 5. 관람가
- lke_obj.ratings = '전체관람가';
- // 6. 예고편
- lke_obj.trailer = function(){
-     console.log('예고편:','3Mi62VKDhm0');
-     
-     //플레이어함수 호출
-     playMovie('3Mi62VKDhm0');
- }/// trailer 함수 /////
+// 임경은님의 오브젝트
+
+const lke_obj = {};
+// 오브젝트 형만 만들고 할당은 아래에서
+// 1. 영화제목
+lke_obj.title = "WALL·E";
+// 2. 감독
+lke_obj.director = "앤드루 스탠튼";
+// 3. 배우
+lke_obj.actor = "벤 버트, 엘리사 나이트외";
+// 4. 장르
+lke_obj.genre = "애니메이션";
+// 5. 관람가
+lke_obj.ratings = "전체관람가";
+// 6. 예고편
+lke_obj.trailer = function () {
+    console.log("예고편:", "3Mi62VKDhm0");
+
+    //플레이어함수 호출
+    playMovie("3Mi62VKDhm0");
+}; /// trailer 함수 /////
 // 안주현님의 오브젝트!
 const an_obj = {};
 // 오브젝트 형과 할당만 하고 속성 및 값은 아래에서
 // 1. 영화제목 : title
-an_obj.title = '리바운드';
+an_obj.title = "리바운드";
 // 2. 감독 : director
-an_obj.director = '장항준';
+an_obj.director = "장항준";
 // 3. 배우
-an_obj.actor = '안재홍, 이신영, 정진운, 김택, 정건주, 김민, 안지호 外';
+an_obj.actor = "안재홍, 이신영, 정진운, 김택, 정건주, 김민, 안지호 外";
 // 4. 장르
-an_obj.genre = '스포츠, 농구, 성장, 코미디';
+an_obj.genre = "스포츠, 농구, 성장, 코미디";
 // 5. 상영등급
-an_obj.ratings = '12세';
+an_obj.ratings = "12세";
 // 6. 예고편
-an_obj.trailer = function(){
-    console.log('예고편','D2RAJe0ZL1Q');
+an_obj.trailer = function () {
+    console.log("예고편", "D2RAJe0ZL1Q");
     // 플레이어 함수 호출!
-    playMovie('D2RAJe0ZL1Q');
-} /////////// trailer함수 ////////////
+    playMovie("D2RAJe0ZL1Q");
+}; /////////// trailer함수 ////////////
 // 김윤민님의 오브젝트
-const kimyoonmin_obj = {
-};
+const kimyoonmin_obj = {};
 
 // 오브젝트 형만 만들고 객체내용은 아래에서!
 // 1. 영화제목
-kimyoonmin_obj.title = '인셉션';
+kimyoonmin_obj.title = "인셉션";
 
 // 2. 감독
-kimyoonmin_obj.director = '크리스토퍼 놀란';
+kimyoonmin_obj.director = "크리스토퍼 놀란";
 
 // 3. 배우
-kimyoonmin_obj.actor = '레오나르도 디카프리오, 조셉 고든 레빗';
+kimyoonmin_obj.actor = "레오나르도 디카프리오, 조셉 고든 레빗";
 
 // 4. 장르
-kimyoonmin_obj.genre = '액션';
+kimyoonmin_obj.genre = "액션";
 
 // 5. 관람가
-kimyoonmin_obj.ratings = '12';
+kimyoonmin_obj.ratings = "12";
 
 // 6. 예고편
-kimyoonmin_obj.trailer = function(){
-        console.log('예고편: ', '1FdB6LPSMkI');
-        playMovie('1FdB6LPSMkI');
+kimyoonmin_obj.trailer = function () {
+    console.log("예고편: ", "1FdB6LPSMkI");
+    playMovie("1FdB6LPSMkI");
 }; /////////////trailer 함수 ////////////////////
 // 최지민님의 오브젝트!
 const cake_obj = {};
 // 오브젝트 형만 만들고 객체내용은 아래에서!
 // 1. 영화제목
-cake_obj.title = '암살';
+cake_obj.title = "암살";
 // 2. 감독
-cake_obj.director = '최동훈';
+cake_obj.director = "최동훈";
 // 3. 배우
-cake_obj.actor = '전지현, 이정재, 하정우, 최덕문, 조진웅';
+cake_obj.actor = "전지현, 이정재, 하정우, 최덕문, 조진웅";
 // 4. 장르
-cake_obj.genre = '액션/드라마';
+cake_obj.genre = "액션/드라마";
 // 5. 관람가
-cake_obj.ratings = '15세 관람가';
+cake_obj.ratings = "15세 관람가";
 
 // 6. 예고편
-cake_obj.trailer = function(){
-    console.log('예고편 :', 'RnGxpZ75zFU');
+cake_obj.trailer = function () {
+    console.log("예고편 :", "RnGxpZ75zFU");
     // 프레이어 함수 호출
-    playMovie('RnGxpZ75zFU');
-} // trailer 함수
- // 문승준님의 오브젝트
- const Moon_Obj = {};
- // 오브젝트 형만 만들고 할당은 아래서
- // 1. 영화제목
- Moon_Obj.title = "어바웃 타임";
- // 2. 감독
- Moon_Obj.director = "리처드 커티스";
- // 3. 출연배우
- Moon_Obj.actor = "도널 글리슨, 레이첼 맥아담스";
- // 4. 장르
- Moon_Obj.genre = "로맨스";
- // 5. 관람가
- Moon_Obj.ratings = "15세";
- // 6. 예고편
- Moon_Obj.trailer = function () {
-     console.log("예고편", "Ugmu8HOdpdo");
-     playMovie("Ugmu8HOdpdo");
- };
- // 최진구님의 오브젝트
- const tonyFilm={};
- //movie info
- //1.title
- tonyFilm.title='창궐';
- //2.director
- tonyFilm.director='김성훈';
- //3.actors
- tonyFilm.actor='현빈,장동건,조우진,기타등등등등등';
- //4.genre
- tonyFilm.genre='액션사극';
- //5.age
- tonyFilm.ratings='15세';
- //6.teaser
- tonyFilm.trailer=function(){
-     console.log('trailer','XvH4t7pvDc0');
- }
- // 유승환님의 오브젝트!
- const yu_obj = {};
- // 오브젝트 형만 만들고 할당은 아래에서!
- // 1. 영화제목
- yu_obj.title = '오만과 편견';
+    playMovie("RnGxpZ75zFU");
+}; // trailer 함수
+// 문승준님의 오브젝트
+const Moon_Obj = {};
+// 오브젝트 형만 만들고 할당은 아래서
+// 1. 영화제목
+Moon_Obj.title = "어바웃 타임";
+// 2. 감독
+Moon_Obj.director = "리처드 커티스";
+// 3. 출연배우
+Moon_Obj.actor = "도널 글리슨, 레이첼 맥아담스";
+// 4. 장르
+Moon_Obj.genre = "로맨스";
+// 5. 관람가
+Moon_Obj.ratings = "15세";
+// 6. 예고편
+Moon_Obj.trailer = function () {
+    console.log("예고편", "Ugmu8HOdpdo");
+    playMovie("Ugmu8HOdpdo");
+};
+// 최진구님의 오브젝트
+const tonyFilm = {};
+//movie info
+//1.title
+tonyFilm.title = "창궐";
+//2.director
+tonyFilm.director = "김성훈";
+//3.actors
+tonyFilm.actor = "현빈,장동건,조우진,기타등등등등등";
+//4.genre
+tonyFilm.genre = "액션사극";
+//5.age
+tonyFilm.ratings = "15세";
+//6.teaser
+tonyFilm.trailer = function () {
+    console.log("trailer", "XvH4t7pvDc0");
+};
+// 유승환님의 오브젝트!
+const yu_obj = {};
+// 오브젝트 형만 만들고 할당은 아래에서!
+// 1. 영화제목
+yu_obj.title = "오만과 편견";
 
- // 2. 감독
- yu_obj.director = '조 라이트';
+// 2. 감독
+yu_obj.director = "조 라이트";
 
- // 3. 배우
- yu_obj.actor = '키이라 나이틀리, 매슈 맥패디언, 켈리 멀리건';
+// 3. 배우
+yu_obj.actor = "키이라 나이틀리, 매슈 맥패디언, 켈리 멀리건";
 
- // 4. 장르
- yu_obj.genre= '로맨스/드라마';
+// 4. 장르
+yu_obj.genre = "로맨스/드라마";
 
- // 5. 관람가
- yu_obj.ratings = '12세';
+// 5. 관람가
+yu_obj.ratings = "12세";
 
- // 6. 예고편
- yu_obj.trailer = function(){
-     console.log('예고편:', 'r7wJ6HG7aCw');
-     // 플레이어 함수 호출!
-     playMovie('r7wJ6HG7aCw');
- }
+// 6. 예고편
+yu_obj.trailer = function () {
+    console.log("예고편:", "r7wJ6HG7aCw");
+    // 플레이어 함수 호출!
+    playMovie("r7wJ6HG7aCw");
+};
