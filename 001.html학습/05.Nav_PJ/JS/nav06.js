6da348d059826ddbad706d4540dd429ea717c064
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-13 16:37:38 +0900

    nav06

--- 001.html학습/05.Nav_PJ/JS/nav06.js
+++ 001.html학습/05.Nav_PJ/JS/nav06.js
@@ -16,5 +16,37 @@ const domFn = {
 // GNB 메뉴의 데이터를 모두 html DOM으로 구조화하여 화면에 출력한다.
 
 // 2. 대상선정 : .gnb
-const gnbBox = domFn.qs('.gnb');
-console.log('대상:',gnbBox);
+const gnbBox = domFn.qs(".gnb");
+console.log("대상:", gnbBox);
+let hcode='';
+// 3. 객체 데이터로 html 코드 만들기
+
+for (let x in mdata) {
+  console.log("속성명:", x);
+  hcode+=
+  `
+  <ul>
+    <li>
+        <a href="">${x}</a>
+        <div class="smenu">
+            <aside class="smbx">
+                <h2>
+                    <div class="stit">2차</div>
+                    <a href="">전체보기</a>
+                    <div class="swrap">
+                        <dl>
+                            <dt></dt>
+                            <dd><a href="">요기</a></dd>
+                        </dl>
+                    </div>
+                </h2>
+            </aside>
+        </div>
+    </li>
+</ul>
+  `;
+} // for in 
+
+// 확인
+console.log('코드:',hcode);
+
