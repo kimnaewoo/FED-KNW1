a5b48d117b749f906d592f80d103d5cd2f5ed8ff
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-14 14:56:30 +0900

    dance.html

---
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -0,0 +1,13 @@
+// 05. 중간스크롤 가로이동 JS - dance.js 
+
+// DOM 함수 객체 //////////////
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// domFn 객체 /////////////
