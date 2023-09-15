8e9c103bd5445e5ef05a6f66b73b9e1f75d3ee69
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-15 13:16:47 +0900

    휠제어 js

--- 003.JS학습/01.JS실험실/js/cube2.js
+++ 003.JS학습/01.JS실험실/js/cube2.js
@@ -1,27 +1,73 @@
-// 큐브 애니2 JS - cube2.js //////////////
+// 회전제어 JS - cube2.js //////////////
 
-/************************************* 
+/*********************************************************************************************************************************************************************** 
     [구현내용]
-    - "돌아!"버튼을 클릭하면 멈춰있던
-    큐브가 돌아감. 이때 버튼이 "멈춰!"
-    버튼으로 변경되어 있음!
-    - "멈춰!" 버튼을 클릭하면 돌고있던
-    큐브가 멈춤. 이때 버튼이 "돌아!"
-    버튼으로 변경되어 있음!
+    - 마우스휠 이벤트에 따라 기본기능은 막고 큐브를 회전하는 속성인 transform의 rotateY(각도)의 값변경을 이용한 큐브 회전을 적용함 
+    - 대상 : window
+    - 사용이벤트 : wheel 
+    - 단위각도 : 360deg / 9(이미지개수) = 40deg 
+    - CSS 이징적용 : ease-out 
+***********************************************************************************************************************************************************************/
 
-*************************************/
 
-// window 로드이벤트 호출
-window.addEventListener("DOMContentLoaded", loadFn);
+// DOM 함수 객체 //////////////
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// domFn 객체 /////////////
 
-// DOM 선택함수
-const qs = (x) => document.querySelector(x);
-const qsa = (x) => document.querySelectorAll(x);
 
-// 로딩함수 ////
-function loadFn() {
-    // 함수호출확인
-    console.log("로딩완료!");
+// 0. 변수셋팅 
+// 단위각도
+const DEG = 40;
+// 광휠상태변수( 0-허용 , 1-금지 )
+let stsWheel = 0;
+// 휠제어시간
+const TIME_WHEEL = 250;
+// 휠단위수(휠할때 증감하는 수)
+let numWheel = 0;
 
-   
-} // loadFn 함수 ////
+
+// 1. 대상선정 : .cube
+const cube = domFn.qs('.cube');  
+console.log('대상',cube);
+
+// 2. 이벤트 설정하기 
+domFn.addEvt(window,'wheel',rotateMem);
+
+// 3. 함수설정
+function rotateMem(){
+
+    // 0. 휠이벤트 발생수 조절하기(광휠금지)
+    if(stsWheel) return; // 막기
+    stsWheel = 1; // 잠금
+    setTimeout(()=>stsWheel=0,TIME_WHEEL); // 해제
+
+    // 1. 휠방향 알아내기 : 휠 Delta값 
+    let delta = event.wheelDelta;
+
+    
+    // 2. 방향에 따른 휠단위수 증감하기
+    if(delta<0){
+        // 휠단위수 증가
+        numWheel++
+    }
+    else
+        // 휠단위수 감소
+        numWheel--
+
+
+    // 호출확인
+    console.log('휠~~~~',delta,numWheel);
+
+    // 3. 회전대상 요소에 각도 적용하기
+    // 적용각도 = 단위각도 * 휠단위수
+    cube.style.transform = `rotateY(${numWheel*DEG}deg)`;
+}; // rotateMem 함수 
+
