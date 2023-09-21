6c2b7f6ac410a338057df670385836d8b51296a1
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-21 14:25:18 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -224,7 +224,9 @@ console.log(preBox);
 // 데이터 : 역순정렬을 한 미리보기 데이터 넣기
 preBox.forEach((ele,idx)=>{
   ele.innerHTML = `
+    <div>
     <h3>${preNewData[idx].title}</h3>
     <p>${preNewData[idx].story}</p>  
+    </div>
   `;
 })
