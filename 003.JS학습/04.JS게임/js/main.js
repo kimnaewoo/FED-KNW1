6ff59c3a86439ca3dc0d84e88b10b1a73541a231
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-09-27 13:14:54 +0900

    대유잼 게임

--- 003.JS학습/04.JS게임/js/main.js
+++ 003.JS학습/04.JS게임/js/main.js
@@ -156,6 +156,30 @@ function whoWinner(){
         let rdmNum = Math.floor(Math.random()*selMsg.length);
         console.log('랜덤수:',rdmNum,'개수:',selMsg.length);
         console.log('랜던메시지:',selMsg[rdmNum]);
+
+        let rdmMsg = selMsg[rdmNum];
+        msg.innerText = rdmMsg;
+        // (6) 메시지 박스 보이기
+        msg.style.display = 'block';
+        msg.style.zIndex = '100';
+
+        // (7) 전체 반투명 커버 암전주기
+        dFn.qs('.cover').innerHTML += `
+        <div 
+            style='
+                position:fixed;
+                top:0;
+                left:0;
+                width:100vw;
+                height:100vh;
+                background-color:#000;
+                opacity:0.5;
+                z-index:99;
+        '></div>`
+
+        // (8) 버튼 위로 올리기
+        dFn.qs('#btns').style.zIndex = "200";
+        
     } // if
     // else if(r1pos >= 600)
     // clearInterval(autoI);
