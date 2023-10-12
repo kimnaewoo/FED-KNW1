b8b1cf7a1bb7527f0742cb29eb893703b039c4fd
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-12 15:20:09 +0900

    Update calendar.js

--- 003.JS학습/06.JS달력/js/calendar.js
+++ 003.JS학습/06.JS달력/js/calendar.js
@@ -11,6 +11,31 @@ const dFn = {
 
 function makeDallyeok(){
     dFn.cg('달력만들어!');
+
+    // 1. 변수셋팅
+    // (1) 변경할 현재날짜 객체
+    const currDate = new Date();
+    // (2) 오늘날짜 객체 
+    const today = new Date();
+    // (3) 년도요소 : .yearTit
+    const yearTit = dFn.qs('.yearTit');
+    // (4) 월요소 : .monthTit
+    const monthTit = dFn.qs('.monthTit');
+    // (5) 일요소 : .dates
+    const dates = dFn.qs('.dates');
+    
+    // dFn.cg(yearTit);
+    // dFn.cg(monthTit);
+    // dFn.cg(dates);
+
+    // 2. 함수 만들기
+    // (1) 달력 초기화구성 함수 
+    const initDallyeok = () => {
+
+        // 1. 전달 마지막 날짜 (옵션:0)
+
+    }; // initDallyeok 함수
+
 } // makeDallyeok 함수
 
 // 달력함수 호출
