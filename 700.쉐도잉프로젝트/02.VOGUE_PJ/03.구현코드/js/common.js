a2600d95f3612c7f1ae5ec36f2e358f6b9c96560
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-17 16:03:44 +0900

    vogue PJ

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
@@ -3,12 +3,10 @@
 // 상단, 하단 공통 데이터 불러오기
 import tData from "./data/com_module.js";
 import dFn from "./dom.js";
-// 부드러운 스크롤 모듈
-import { startSS, setPos } from "./smoothScroll23.js";
 
 
-// [1] 부드러운 스크롤 적용 //////////
-startSS();
+
+
 
 // [2] 상단/하단 공통 모듈 넣기 
 
