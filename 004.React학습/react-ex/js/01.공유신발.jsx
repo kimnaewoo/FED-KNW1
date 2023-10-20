df80c9f5603d7030f5a2c23e4ec262d16c85ae22
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-20 15:43:52 +0900

    react

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -10,26 +10,36 @@ const twoData = [myData,myData2];
 // 메인 컴포넌트
 // 메인의 의미는 ? 다른 구성요소 컴포넌트들을 모아 최종적으로 렌더링하는 구성 컴포넌트를 말한다.
 function MainComponent(){
+    
+    // 상태관리 메서드를 사용하여 후크변수를 설정하자!
+    // const [변수명,set변수명] = React.useState(초기값)
+    // dataNum은 데이터를 구분하는 번호저장 후크변수다!
+    // 데이터 구분값으로 배열순번을 생각하여 처음에 로딩될 데이터가 0번째 즉, 첫번째 배열순번 데이터를 값으로 셋팅한다!
+    const [dataNum,setDataNum] = React.useState(0);
 
+    console.log('최초값:',dataNum);
 
     // 데이터 변경호출 함수 
     const chgData = () => {
         console.log('바꿔~!');
+        // 데이터 키 후크변수를 업데이트함 
+        setDataNum(dataNum?0:1);
+        console.log('업데이트값:',dataNum);
     }; // chgData 함수 
 
     return(
         <React.Fragment>
            {/* 1. 타이틀  */}
-           <h1 className="tit">공유가 신고 다닌다는!</h1>
+           <h1 className="tit">{dataNum?'효진이 입고':'공유가 신고'} 다닌다는!</h1>
            {/* 2. 내용박스 */}
             <section> 
-                <h2>공유는 오늘도 멋집니다!</h2>
+                <h2>{dataNum?'김효진말고 공효진~':'공유발은 지네발~'} </h2>
                 {/* 이미지 */}
-                <img src="./images/vans/gongyoo.jpg" alt="공유"/>
+                <img src={dataNum?'https://www.sisanews.kr/news/photo/201601/16647_13007_488.jpg':'./images/vans/gongyoo.jpg'} alt={dataNum?"효진":"공유"}/>
             </section>
             {/* 3. 데이터 변경 버튼 */}
             <button onClick={chgData} className="btn-gong">
-                효진초이스 바로가기
+                {dataNum?'공유초이스 바로가기':'효진초이스 바로가기'}
             </button>
             {/* 4. 상품리스트박스 */}
             <div className="gwrap">
