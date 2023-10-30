b503677012ec347bd6bee68c7d6ee19bb7b0c241
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-30 15:12:51 +0900

    vogue PJ - login

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
@@ -1,1 +1,43 @@
 // 보그 PJ 회원가입 페이지 JS - member.js
+
+/********************************************************************************** 
+
+    [ 사용자 입력폼 유효성 검사 ]
+    - 이벤트 종류 : blur(포커스가 빠질때 발생)
+    -제이쿼리 이벤트 메서드 : blur()
+    - 이벤트 대상 : 입력요소 중 text(id!=email2제외),password
+        .form.logF input[type=text][id!=email2],
+        .form.logF input[type=password],
+        -> 요소뒤 대괄호는 속성선택자(CSS원래문법)
+        [id!=email2] != 같지않다(제이쿼리문법)
+
+
+**********************************************************************************/
+$(`form.logF input[type=text][id!=email2],
+form.logF input[type=password]`)
+  // .on('blur',function(){})
+  .blur(function () {
+    // 1. 현재 블러가 발생한 요소의 아이디는 ?
+    let cid = $(this).attr("id");
+    // cid 는 current id 즉, 현재 아이디
+
+    // 2. 현재 블러가 발생한 요소의 값은 ?
+    let cv = $(this).val();
+
+    console.log("id는?", cid, "/값은?", cv);
+
+    /***************************************** 
+        3. 빈값 여부 검사하기 (필수입력항목)
+    *****************************************/
+    if (cv == "") {
+        // 메시지 출력하기
+        $(this).siblings('.msg').text('필수입력!');
+    } // if
+
+    // 모두 통과일 경우 메시지 지우기 - empty() 내용을 지우는 메서드 
+    else{
+        $(this).siblings('.msg').empty();
+    } // else 
+
+
+  }); // blur
