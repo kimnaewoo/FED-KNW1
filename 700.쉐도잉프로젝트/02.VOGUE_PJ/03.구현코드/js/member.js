6514a8f6a50028a2d44fa041c729c5984f694144
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-10-31 14:11:59 +0900

    s

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
@@ -22,7 +22,28 @@ chkAll.change(function(){
   // 1. 체크박스 체크여부 확인하기 
   let isChk = $(this).prop('checked');
   console.log('체크양?',isChk);
+
+  // 2. 전체 체크박스가 체크상태 (true)이면 개별박스 모두 true로 변경 처리, 미체크 상태(false)이면 개별박스 모두 false 처리
+  chkEach.prop('checked', isChk);
+  // chkEach.attr('checked', isChk);
+  // attr() 또한, for문 없이도 여러개의 체크박스를 동시에 변경할 수 있다. 하지만 동일하게 변경도 가능하나, 읽어오지는 못한다. 
+
 }); // change
+
+/************************************************************** 
+    약관동의 개별 체크시 전체 체크박스 변경하기 
+**************************************************************/
+// 원리 : 개별체크박스가 모두 체크되면 전체체크하기
+// 대상 : .chkEach -> chkEach 변수
+chkEach.change(function(){
+  // 1. 체크 개수 알아오기 : length -> 개수 리턴! 
+  let num = $('.chk:checked').length
+  console.log('체크개수',num);
+
+  // 2. 체크개수가 3이면 전체체크박스 체크하기
+  if(num==3) chkAll.prop('checked',true);
+  else chkAll.prop('checked',false);
+}); // change 
 
 /* 
   [ 속성값을 읽어오는 메서드 2가지 ]
