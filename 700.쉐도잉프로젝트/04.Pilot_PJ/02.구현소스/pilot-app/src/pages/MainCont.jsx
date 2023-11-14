206f980cfdf09ffcdf10221a2d758af6d08d0f3b
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-14 15:48:39 +0900

    Pilot PJ

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -1,10 +1,27 @@
 // 메인 페이지 컨텐츠 컴포넌트
 
+import { Banner } from "../modules/Banner";
+
 export function MainCont(){
 
     return(
         <>
-            <h2>메인 컨텐츠야~!</h2>
+            {/* 1. 배너페이지 */}
+            <section className="page" style={{background:'lightblue'}}>
+                <Banner />
+            </section>
+            <section className="page" style={{background:'lightsalmon'}}>
+
+            </section>
+            <section className="page" style={{background:'lightgreen'}}>
+
+            </section>
+            <section className="page" style={{background:'lightcoral'}}>
+
+            </section>
+            <section className="page" style={{background:'lightpink'}}>
+
+            </section>
         </>
     )
 
