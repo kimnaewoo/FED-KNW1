76271ba757253febee8b17d5864f9d2bb1405926
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-16 10:57:47 +0900

    DC.com PJ

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -55,7 +55,7 @@ export function TopArea() {
             {/* 3. 검색,회원가입,로그인 링크 */}
             <li style={{marginLeft:'auto'}}>
               {/* 검색기능링크 - 클릭시 검색창보이기 */}
-              <a hreft="#" onClick="">
+              <a hreft="#">
                   <FontAwesomeIcon icon={faSearch} />
                   {/* <FontAwesomeIcon icon={faCamera} */}
               </a>
@@ -69,7 +69,7 @@ export function TopArea() {
             </li>
           </ul>
           {/* 모바일용 햄버거 버튼 */}
-          <button className="hambtn" onClick=""></button>
+          <button className="hambtn"></button>
         </nav>
       </header>
     </>
