982b2e18dca919f1a25a08f09fd4b904d1752ed1
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-11-20 15:20:05 +0900

    Update index.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,4 +1,4 @@
-// 메인페이지 JS - index.js 
+// 메인페이지 JS - index.js
 import React, { useEffect, useState } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import { TopArea } from "./layout/TopArea";
@@ -7,46 +7,49 @@ import { FooterArea } from "./layout/FooterArea";
 
 // 제이쿼리 불러오기
 import $ from "jquery";
-import 'jquery-ui-dist/jquery-ui';
+import "jquery-ui-dist/jquery-ui";
 
 // 페이지 공통 CSS
-import './css/common.css';
+import "./css/common.css";
 
 // 최상위 Root 컴포넌트 //////
-function App(){
-
+function App() {
   // 후크상태변수 설정 : 페이지변경
-  const [pgName,setPgName] = useState('main');
+  const [pgName, setPgName] = useState("main");
 
-  // 페이지변경 상태변수 업데이트 함수 
+  // 페이지변경 상태변수 업데이트 함수
   const chgPgName = (txt) => {
     setPgName(txt);
   }; // chgPgName
 
   // 랜더링 후 실행구역
-  useEffect(()=>{
-    $('.gnb li').click(function(){
+  useEffect(() => {
+    $(".gnb li, .indic li").click(function () {
       // 순번변수
       let idx = $(this).index();
-      console.log('나야나~!',idx)
+      console.log("나야나~!", idx);
       // 페이지 이동
-      $('html,body').animate({
-        scrollTop:($(window).height()*idx)+'px'
-      },800,'easeInOutQuint')
-    })
+      $("html,body").animate(
+        {
+          scrollTop: $(window).height() * idx + "px",
+        },
+        800,
+        "easeInOutQuint"
+      );
+      $('.gnb a').addClass("on").eq(idx).siblings().removeClass("on");
+      $('indic li').addClass("on").eq(idx).siblings().removeClass("on");
+    });
   }); // useEffect
 
-
   // 리턴 코드
-  return(
+  return (
     <>
-      <TopArea cat={pgName}/>
+      <TopArea cat={pgName} />
       <MainArea page={pgName} />
-      <FooterArea/>
+      <FooterArea />
     </>
-  )
-    
-} // App 컴포넌트 
+  );
+} // App 컴포넌트
 
 /* 
   <button onClick={()=>chgPgName('main')}>메인페이지</button>
@@ -56,5 +59,5 @@ function App(){
 */
 
 // 출력하기
-const root = createRoot(document.querySelector('#root'));
-root.render(<App />)
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
