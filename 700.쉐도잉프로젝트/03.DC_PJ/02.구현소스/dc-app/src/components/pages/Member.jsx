8a12310bf64ca1f53c4459030ac727e4fa1d2b96
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-12-01 12:09:04 +0900

    Update Member.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Member.jsx
@@ -34,11 +34,115 @@ export function Member() {
   // 5. 이메일 변수
   const [emailError, setEmailError] = useState(false);
 
-  // [아이디관련 메시지 프리셋]
+  // [ 아이디관련 메시지 프리셋 ]
   const msgId = ["User ID must contain a minimum of 5 characters", "This ID is already in use!", "That's a great ID!"];
 
-  // [3] 에러메시지 상태변수 
-  const [idMsg,setIdMsg] = useState(msgId[0]);
+  // [ 기타 메시지 프리셋 ]
+  const msgEtc = {
+    // 비밀번호
+    pwd:"5 to 15 digits in the form of special characters, characters, and numbers",
+    // 비밀번호확인
+    confpwd:"Password verification does not match",
+    // 필수입력
+    req:"This is a required entry",
+    // 이메일
+    email:"Please enter a valid email format"
+
+}; // msgEtc
+
+  // [3] 에러메시지 상태변수
+  const [idMsg, setIdMsg] = useState(msgId[0]);
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // [ 유효성 검사 함수]
+  // 1. 아이디 유효성 검사
+  const changeUserId = (e) => {
+    // 1. 아이디 유효성 검사식(따옴표로 싸지말것!!!)
+    const valid = /^[A-Za-z0-9+]{5,}$/;
+
+    // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
+    // console.log(e.target.value);
+
+    // 3. 에러아님 상태 if문
+    // 조건 : 유효성검사 결과가 true인가? 에러상태가 아니면 false
+    // 검사방법 : 정규식.test() -> 정규식 검사결과 리턴 메서드
+    // 결과 : true이면 에러상태값 false (false이면 에러상태값 true)
+    // 에러상태가 아닐때
+    if (valid.test(e.target.value)) {
+      // 1. 사용중 아이디인지 검사(로컬쓰 셋팅후 추가!)
+
+      // 2. 결과 반영하기
+      setUserIdError(false);
+    } // if
+    // 에러상태
+    else {
+      setUserIdError(true);
+    } // else
+
+    // 4. 실제 userId 상태변수값이 업데이트 되어야만 화면에 출력
+    setUserId(e.target.value);
+  }; // changeUseId 함수
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // 2. 비밀번호 유효성 검사
+  const changePwd = (e) => {
+    // 1. 비밀번호 유효성 검사식(따옴표로 싸지말것!!!)
+    const valid = /^.*(?=^.{5,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+
+    // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
+    console.log(e.target.value);
+
+    // 3. 에러에 따른 상태값 변경
+    if (valid.test(e.target.value)) setPwdError(false);
+    else setPwdError(true);
+
+    // 4. 기존입력값 반영하기
+    setPwd(e.target.value);
+  }; // changeUseId 함수
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // 3. 비밀번호확인 유효성 검사
+  const changeChkPwd = (e) => {
+    // 1. 비밀번호 입력내용과 일치여부 확인 
+    if (pwd===e.target.value) setChkPwdError(false);
+    else setChkPwdError(true);
+
+    // 2. 기존입력값 반영하기
+    setChkPwd(e.target.value);
+  }; // changeUseId 함수
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // 4. 사용자이름 유효성 검사
+  const changeUserName = (e) => {
+    // 1. 빈값체크 확인 
+    if (e.target.value!=="") setUserNameError(false);
+    else setUserNameError(true);
+
+    // 2. 기존입력값 반영하기
+    setUserName(e.target.value);
+  }; // changeUseId 함수
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 5. 이메일 유효성 검사
+const changeEmail = (e) => {
+    // 1. 이메일 유효성 검사식(따옴표로 싸지말것!!!)
+    const valid = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    // 2. 입력값 확인 : e.target -> 이벤트가 발생한 요소
+    console.log(e.target.value);
+
+    // 3. 에러에 따른 상태값 변경
+    if (valid.test(e.target.value)) setEmailError(false);
+    else setEmailError(true);
+
+    // 4. 기존입력값 반영하기
+    setEmail(e.target.value);
+  }; // changeUseId 함수
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -53,27 +157,91 @@ export function Member() {
             <li>
               {/* 1. 아이디 */}
               <label>ID : </label>
-              <input type="text" maxLength="20" placeholder="Please enter your ID"></input>
+              <input
+                type="text"
+                maxLength="20"
+                placeholder="Please enter your ID"
+                value={userId}
+                onChange={changeUserId}
+              />
+              {
+                // 에러가 맞을때 메시지 출력
+                // 조건문 && 요소
+                userIdError && (
+                  <div className="msg">
+                    <small style={{ color: "red", fontSize: "10px" }}>{idMsg}</small>
+                  </div>
+                )
+              }
+              {
+                // 에러가 아닐때 메시지 출력
+                // 조건문 && 요소
+                // 조건추가 : userId가 입력전일때 안보임 처리
+                // userId가 입력전에는 false로 리턴됨!
+                !userIdError && userId && (
+                  <div className="msg">
+                    <small style={{ color: "green", fontSize: "10px" }}>{msgId[2]}</small>
+                  </div>
+                )
+              }
             </li>
             <li>
               {/* 2. 비밀번호 */}
               <label>Password : </label>
-              <input type="password" maxLength="20" placeholder="Please enter your Password"></input>
+              <input
+                type="password"
+                maxLength="20"
+                placeholder="Please enter your Password"
+                value={pwd}
+                onChange={changePwd}
+              />
+              {
+                // 에러시 메시지 출력
+                pwdError && (
+                  <div className="msg">
+                    <small style={{ color: "red", fontSize: "10px" }}>{msgEtc.pwd}</small>
+                  </div>
+                )
+              }
             </li>
             <li>
               {/* 3. 비밀번호 학인 */}
               <label>Confirm Password : </label>
-              <input type="password" maxLength="20" placeholder="Please enter your Confirm Password"></input>
+              <input type="password" maxLength="20" placeholder="Please enter your Confirm Password" value={chkpwd} onChange={changeChkPwd}/>
+              {
+                // 에러시 메시지 출력
+                chkpwdError && (
+                  <div className="msg">
+                    <small style={{ color: "red", fontSize: "10px" }}>{msgEtc.confpwd}</small>
+                  </div>
+                )
+              }
             </li>
             <li>
               {/* 4. 이름 */}
               <label>User Name : </label>
-              <input type="text" maxLength="20" placeholder="Please enter your Name"></input>
+              <input type="text" maxLength="20" placeholder="Please enter your Name" value={userName} onChange={changeUserName}/>
+              {
+                // 에러시 메시지 출력
+                userNameError && (
+                  <div className="msg">
+                    <small style={{ color: "red", fontSize: "10px" }}>{msgEtc.req}</small>
+                  </div>
+                )
+              }
             </li>
             <li>
               {/* 5. 이메일 */}
               <label>E-mail : </label>
-              <input type="text" maxLength="50" placeholder="Please enter your E-mail"></input>
+              <input type="text" maxLength="50" placeholder="Please enter your E-mail" value={email} onChange={changeEmail}/>
+              {
+                // 에러시 메시지 출력
+                emailError && (
+                  <div className="msg">
+                    <small style={{ color: "red", fontSize: "10px" }}>{msgEtc.email}</small>
+                  </div>
+                )
+              }
             </li>
             <li style={{ overflow: "hidden" }}>
               {/* 6. 버튼 */}
