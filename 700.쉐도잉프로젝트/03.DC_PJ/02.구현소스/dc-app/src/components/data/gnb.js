936beaf6f6b6706d75ab31b633782d74abab2d6e
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-12-04 10:58:09 +0900

    npm build

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/data/gnb.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/data/gnb.js
@@ -1,59 +1,59 @@
-// DC.com GNB메뉴 데이터 
+// DC.com GNB메뉴 데이터
 export const menu = [
-    {
-        txt:"CHARACTERS",
-        link:"/characters",
-    },
-    {
-        txt:"COMICS",
-        link:"/comics",
-        sub:[
-            {
-                txt: "LATEST COMICS",
-                link: "/comics",
-            },
-            {
-                txt: "DC UNIVERSE INFINITE",
-                link: "/comics",
-            },
-            {
-                txt: "ALL COMICS SERIES",
-                link: "/comics",
-            },
-        ],
-    },
-    {
-        txt:"MOVIES & TV",
-        link:"/movies",
-        sub:[
-            {
-                txt: "DC MOVIES",
-                link: "/movies",
-            },
-            {
-                txt: "DC SERIES",
-                link: "/series",
-            },
-            {
-                txt: "DC ON HBO MAX",
-                link: "/mv",
-            },
-        ]
-    },
-    {
-        txt:"GAMES",
-        link:"/games",
-    },
-    {
-        txt:"NEWS",
-        link:"/news",
-    },
-    {
-        txt:"VIDEO",
-        link:"/video",
-    },
-    {
-        txt:"OPNIONS",
-        link:"/board",
-    },
+  {
+    txt: "CHARACTERS",
+    link: "/characters",
+  },
+  {
+    txt: "COMICS",
+    link: "/comics",
+    sub: [
+      {
+        txt: "LATEST COMICS",
+        link: "/comics",
+      },
+      {
+        txt: "DC UNIVERSE INFINITE",
+        link: "/comics",
+      },
+      {
+        txt: "ALL COMICS SERIES",
+        link: "/comics",
+      },
+    ],
+  },
+  {
+    txt: "MOVIES & TV",
+    link: "/movies",
+    sub: [
+      {
+        txt: "DC MOVIES",
+        link: "/movies",
+      },
+      {
+        txt: "DC SERIES",
+        link: "/series",
+      },
+      {
+        txt: "DC ON HBO MAX",
+        link: "/mv",
+      },
+    ],
+  },
+  {
+    txt: "GAMES",
+    link: "/games",
+  },
+  {
+    txt: "NEWS",
+    link: "/news",
+  },
+  {
+    txt: "VIDEO",
+    link: "/video",
+  },
+  {
+    txt: "OPNIONS",
+    link: "/board",
+  },
 ];
