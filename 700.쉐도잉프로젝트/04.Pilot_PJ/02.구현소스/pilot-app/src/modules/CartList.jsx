11d3ca73ebe6f2f5b4481bd08b48e5a0889d838d
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-12-13 13:17:33 +0900

    Pilot PJ setup

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -19,6 +19,14 @@ export const CartList = memo(({selData}) => {
 
   console.log(selData,cntData+'개');
 
+  // 전체합계 구하기 
+  let totalCnt = 0;
+  selData.forEach(v=>{
+    totalCnt += v.ginfo[3]*v.num;
+  }); // forEach
+
+  console.log('토탈값',totalCnt);
+
   //정규식함수(숫자 세자리마다 콤마해주는 기능)
   function addComma(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -48,6 +56,21 @@ export const CartList = memo(({selData}) => {
     e.preventDefault();
     $("#cartlist").animate({right:'-60%'},600);
   }; //////////// hideList ////////////
+
+  // 리스트 삭제 함수
+  const deleteItem = (e) => {
+    const selIdx = $(e.target).attr('data-idx');
+    console.log('지울녀석',selIdx);
+
+    // 해당 데이터 순번 알아내기
+    const newData = selData.filter((v)=>{if(v.idx!==selIdx){return true}})
+
+    console.log(newData);
+
+    // 선택배열 데이터 selData에서 해당 idx를 삭제함 
+    selData.splice();
+
+  }; // deleteItem 함수
 
   /// 리턴 코드 ///////////////////////
   return (
@@ -96,7 +119,7 @@ export const CartList = memo(({selData}) => {
                 <td>{addComma(v.ginfo[3] * v.num)}원</td>
                 {/* 삭제버튼 */}
                 <td>
-                  <button className="cfn" data-idx={v.idx}>
+                  <button className="cfn" data-idx={v.idx} onClick={deleteItem}>
                     ×
                   </button>
                 </td>
@@ -105,7 +128,7 @@ export const CartList = memo(({selData}) => {
 
             <tr>
               <td colSpan="6">총합계 :</td>
-              <td>999,000원</td>
+              <td>{addComma(totalCnt)}원</td>
               <td></td>
             </tr>
           </tbody>
