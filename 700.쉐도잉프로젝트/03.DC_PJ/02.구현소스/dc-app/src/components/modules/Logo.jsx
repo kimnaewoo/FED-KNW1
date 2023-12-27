ba69d200b5a23b667987fa986a6fc86ebdd6784b
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-12-27 09:32:15 +0900

    Update Logo.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Logo.jsx
@@ -8,62 +8,63 @@ import { dcCon } from "./dcContext";
 import { useContext } from "react";
 
 export const Logo = (props) => {
-    // props.logoStyle : 상단,하단구분 로고코드
-    
-    // 컨텍스트 API 사용하기
-    const myCon = useContext(dcCon);
+  // props.logoStyle : 상단,하단구분 로고코드
 
-    // 라우터 이동메서드 셋팅하기 : useNavigate()
-    // const goNav = useNavigate(); -> 컨텍스트 API사용!
-    // 사용법: 반드시 useNavigate()메서드를 변수에 담아
-    // 이동할 라우터 주소를 쓰면 이동한다
-    // 예) goNav('/news') -> 뉴스페이지이동
-    // 예) goNav('/') -> 첫페이지이동
-    // 이동주소는 대소문자 구분없음!
+  // 컨텍스트 API 사용하기
+  const myCon = useContext(dcCon);
 
+  // 라우터 이동메서드 셋팅하기 : useNavigate()
+  // const goNav = useNavigate(); -> 컨텍스트 API사용!
+  // 사용법: 반드시 useNavigate()메서드를 변수에 담아
+  // 이동할 라우터 주소를 쓰면 이동한다
+  // 예) goNav('/news') -> 뉴스페이지이동
+  // 예) goNav('/') -> 첫페이지이동
+  // 이동주소는 대소문자 구분없음!
 
-    // 객체형 스타일 적용
-    const myStyle = {
-        top:{
-            width:"45px",
-            height:"45px",
-            marginRight:"30px",
-            borderRadius:"50%",
-            cursor:"pointer"
-        },
+  // 객체형 스타일 적용
+  const myStyle = {
+    top: {
+      width: "45px",
+      height: "45px",
+      marginRight: "30px",
+      borderRadius: "50%",
+      cursor: "pointer",
+    },
 
-        bottom:{
-            height: "80px"
-        }
-    };
+    bottom: {
+      height: "80px",
+    },
+  };
 
-    // 이미지 스타일 변경 객체
-    const myStyleImg = {
-        top: "45px",
-        bottom: "80px"
-    }
+  // 이미지 스타일 변경 객체
+  const myStyleImg = {
+    top: "45px",
+    bottom: "80px",
+  };
 
-    // 자식컴포넌트 처리용함수 -> 컨텍스트 API사용!
-    // const nayaLogo = (txt) => {
-    //     // console.log(txt);
-    //     // 라우터 이동하기
-    //     goNav(txt);
-    // }; ///////// nayaLogo //////////
+  // 자식컴포넌트 처리용함수 -> 컨텍스트 API사용!
+  // const nayaLogo = (txt) => {
+  //     // console.log(txt);
+  //     // 라우터 이동하기
+  //     goNav(txt);
+  // }; ///////// nayaLogo //////////
 
-    // 코드 리턴 //////////////////////////
-    return (
-        <h1 
-            style={myStyle[props.logoStyle]}
-            onClick={()=>
-                // 컨텍스트 API 함수호출!
-                myCon.chgPage('/',{})}>
-                <img   
-                    src={process.env.PUBLIC_URL+isrc.logo} 
-                    alt="DC logo"
-                    style={{
-                        width: myStyleImg[props.logoStyle]
-                    }} />
-        </h1>
-    );
-
+  // 코드 리턴 //////////////////////////
+  return (
+    <h1
+      style={myStyle[props.logoStyle]}
+      onClick={() =>
+        // 컨텍스트 API 함수호출!
+        myCon.chgPage("/", {})
+      }
+    >
+      <img
+        src={process.env.PUBLIC_URL + isrc.logo}
+        alt="DC logo"
+        style={{
+          width: myStyleImg[props.logoStyle],
+        }}
+      />
+    </h1>
+  );
 }; ////////////// Logo ///////////////
