311f23c4fcf9ee8c29461ad4724d23d6d2c73973
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2023-12-27 16:33:06 +0900

    DC PJ Weather.jsx setup

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Weather.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Weather.jsx
@@ -1,4 +1,8 @@
 import { Component } from "react";
+// css 불러오기
+import "../../css/weather.css";
+// 엑시오스 불러오기
+import axios from "axios";
 
 // 여기서는 컴포넌트를 class로 만들어보자
 // 컴포넌트 class는 기본적으로 Component 클래스를 상속받는다!
@@ -13,7 +17,7 @@ class Weather extends Component {
     // 클래스 내부속성은 this키워드를 사용한다!
     // 받아온 날씨 정보를 셋업할 객체이다!
     // state 이름의 상태변수에 할당후 setState()로 셋팅한다
-    this.state = { temp: "", desc: "", icon: "", loading: true };
+    this.state = { temp: "", desc: "", icon: "", loading: true, city: "" };
   } // 생성자함수
 
   // 컴포넌트 생성후 날씨정보 조회하여 화면에 보이기
@@ -30,30 +34,83 @@ class Weather extends Component {
     // 소스샘플
     // https://openweathermap.org/api/one-call-3
 
-    // fetch() 함수를 이용한 데이터 조회하기!
-    fetch(url) // 파일 받기
-      .then((res) => res.json()) // json() 제이슨 파일 형식 파싱
-      .then((wdata) => {
-        // 파일 받은후 내용읽기
-        console.log(wdata, wdata.main.temp);
-        // 상태변수인 wInfo에 값을 셋팅한다!
-        // 셋팅용 상태변수 메서드형은 setState()
-        // this키워드 사용
+    // axios 라이브러리를 이용한 데이터 조회하기!
+    // 먼저설치 : npm i axios
+    // 엑시오스는 데이터를 Promise로 처리하여 원하는 결과를 보장하는 간편한 데이터 처리 라이브러리!
+    // 엑시오스 사용법 : 상단에 import 로 불러들인다.
+    // 파일 가져오기 메서드 : get()
+    // 다음 실행 메서드는 : then
+    axios
+      .get(url) // 파일 로딩
+      .then((res) => {
+        // 파일형식에 맞는 파싱 자동
+        console.log(res);
+        // 주의 : 로그에서 확인한바와 같이
+        // data속성에 실제 데이터가 담긴다!
+        // 들어오는 변수인 res.data 해야한다
+        const wdata = res.data;
+
+        // 상태변수값에 셋팅하기
         this.setState({
+          city: cityName,
           temp: wdata.main.temp,
           desc: wdata.weather[0].description,
           icon: wdata.weather[0].icon,
           loading: false, // 로딩여부 끝 (false)
         });
-      }) // 마지막 then
-      // 에러시 처리
+      }) // then
+      // 에러처리 메서드: catch()
       .catch((err) => console.log(err));
+
+    /********************************************************************************************************************************************************************
+     // fetch() 함수를 이용한 데이터 조회하기!
+    // fetch(url) // 파일 받기
+    //   .then((res) => res.json()) // json() 제이슨 파일 형식 파싱
+    //   .then((wdata) => {
+    //     // 파일 받은후 내용읽기
+    //     console.log(wdata, wdata.main.temp);
+    //     // 상태변수인 wInfo에 값을 셋팅한다!
+    //     // 셋팅용 상태변수 메서드형은 setState()
+    //     // this키워드 사용
+    // this.setState({
+    //       city: cityName,
+    //       temp: wdata.main.temp,
+    //       desc: wdata.weather[0].description,
+    //       icon: wdata.weather[0].icon,
+    //       loading: false, // 로딩여부 끝 (false)
+    //     });
+    //   }) // 마지막 then
+    //   // 에러시 처리
+    //   .catch((err) => console.log(err)); 
+    ********************************************************************************************************************************************************************/
   } // componentDidMount 메서드
 
   // 컴포넌트 결과 랜더링 메서드
   // render()
   render() {
-    return <h1>날씨양~!</h1>;
+    // 아이콘 정보
+    const isrc = `http://openweathermap.com/img/w/${this.state.icon}.png`;
+    // 로딩중 loading값이 true이면
+    if (this.state.loading) {
+      return <h4>loading</h4>;
+    } // if
+    // 로딩성공 시 (loading값이 false인경우)
+    else {
+      {
+        /* 절대온도이므로 섭씨온도로 바꾼다! 절대온도 - 273.15를 뺴준다. 소수점도 한자리만 표시 */
+      }
+      let ctemp = (parseInt(this.state.temp) - 273.15).toFixed(1);
+      return (
+        <div className="weather-bx">
+          <h4>Now Weather</h4>
+          <h5>{this.state.city}</h5>
+          <img src={isrc} alt="weather icon" />
+
+          <p>{ctemp} ℃</p>
+          <p>{this.state.desc}</p>
+        </div>
+      );
+    } // else
   } // render 메서드
 } // Weather 클래스
 
