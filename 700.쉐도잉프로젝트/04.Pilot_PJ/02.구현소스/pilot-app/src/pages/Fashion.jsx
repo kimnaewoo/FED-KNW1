eb8ffa030740344f77fc94b4fc1834150a5e3146
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-01-12 09:40:00 +0900

    Pilot PJ 스크롤이벤트 추가

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -1,6 +1,6 @@
 // 공통패션 서브페이지 컨텐츠 컴포넌트
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useLayoutEffect, useReducer, useState } from "react";
 
 // 공통 서브 CSS 불러오기
 import "../css/fashion.css";
@@ -14,36 +14,84 @@ import $ from "jquery";
 import { SinSang } from "../modules/SinSang";
 import { ItemDetail } from "../modules/ItemDetail";
 
+// 부드러운 스크롤 JS
+import { scrolled, setPos } from "../func/smoothScroll24";
+
 export function Fashion(props) {
   // 컨텍스트 API 사용!
   const myCon = useContext(pCon);
 
   // props.cat - 서브 카테고리명
+  console.log('패션페이지cat:',props.cat);
 
   useEffect(() => {
-    // 스크롤바 생성하기
-    $("html,body").css({ overflow: "visible", overflowX: "hidden" });
+    // [부드러운 스크롤 함수 이벤트 설정하기]
+    document.addEventListener("mousewheel", scrolled, {
+      passive: false,
+    });
+    // 이전버전 파이어폭스 브라우저 이벤트처리
+    document.addEventListener("DOMMouseScroll", scrolled, {
+      passive: false,
+    });
+    // 이벤트 설정시 passive:false 설정의 이유는
+    // 기본 설정값은 true이고 이것은 window,document,body
+    // 이 세가지에 preventDefault() 기본작동막기를 할 경우
+    // 이것을 사용할 수 없도록 설정된 값이 treu다!
+    // passive모드를 false로 꺼놔야 window,document,body
+    // 에 대한 기본 막기가 가능함!(여기서는 스크롤 기능임!)
+
+    // 부드러운 스크롤 위치값 초기화!!!
+    setPos(0);
+
+    // 스크롤바 생성하기(x축은 숨김)
+    $("html,body").css({
+      overflow: "visible",
+      overflowX: "hidden",
+    });
 
     // 로고클릭시 페이지이동 : pgName 변경 -> chgPgName()
     $("#logo a").click(() => myCon.chgPgName("main"));
 
-    
-  }, []); // useEffect
+    // 상품상세보기 박스 처음에 숨기기
+    // $(".bgbx").hide();
 
-  // 후크상태 변수
-  const [item, setItem] = useState('m1');
+    // 소멸자 구역 //////////////
+    return () => {
+      // 컴포넌트 소멸시 이벤트 삭제
+      // 부드러운 스크롤 함수 이벤트 삭제하기
+      document.removeEventListener("mousewheel", scrolled, {
+        passive: false,
+      });
+      document.removeEventListener("DOMMouseScroll", scrolled, {
+        passive: false,
+      });
 
-  // 신상컴포넌트에서 상세 컴포넌트로 값을 전달하기 위한 상태변수를 셋팅하여 함수로 이것을 변경하게 해준다
+      // 부드러운 스크롤 위치값 초기화!!!
+      setPos(0);
+    }; /////// 소멸자 ////////////////
+  }, []); ///////// useEffect ///////////
+
+  useLayoutEffect(()=>{
+    setPos(0);
+  },[props.cat])
+
+  
+
+  // 후크 상태변수
+  const [item, setItem] = useState("m1");
+
+  // 신상컴포넌트에서 상세컴포넌트로 값을 전하기 위한
+  // 상태변수를 셋팅하여 함수로 이것을 변경하게 해준다!
   // 프롭스 펑션다운~!!
   const chgItem = (v) => {
-    console.log("상품정보전달:", v);
+    console.log("상품정보:", v);
     // 상태변수 업데이트
     setItem(v);
-    // 상태박스 보이기 
-    $('.bgbx').slideDown(400);
-    
-  };
-  // 리턴코드
+    // 상세박스 슬라이드 애니로 보이기
+    $(".bgbx").slideDown(400);
+  }; /////////// chgItem 함수 //////
+
+  // 리턴코드 //////////////////////////
   return (
     <>
       {/* 1. 배너영역 */}
