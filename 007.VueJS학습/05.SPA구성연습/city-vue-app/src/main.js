0e14063ac52e10f0cb4b92ec7c5a0e183ae2565e
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-01-26 17:53:30 +0900

    update city-vue-app

--- 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
@@ -1,13 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
+// 뷰엑스 스토어 JS 불러오기
+import store from "./store";
+// import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
+  store, // 스토어등록
+  // router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>",
+  // Vue 인스턴스 생성 직후 데이터 초기화
+  created: function () {
+    store.commit("initSet");
+  },
+});
